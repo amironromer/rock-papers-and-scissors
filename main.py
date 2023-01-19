61ef443562574440f25e13cd21a0351e72947ecd
Alejandro Miron <113551832+amironromer@users.noreply.github.com>
2023-01-19 20:32:00 +0000

    game completed

--- main.py
+++ main.py
@@ -2,20 +2,45 @@ import random
 choices = ["rock","paper","scissors"]
 computer= random.choice(choices)
 player= None
-while player not in choices:
-  player=input("rock, paper, and scissors?: ").lower()
-  print(computer)
-  print(player)
-if player==computer:
-  print(computer)
-  print(player)
-  print("TIE!!!!!!!")
-elif player=="rock":
-  if computer=="paper":
+while True:
+  
+  while player not in choices:
+    player=input("rock, paper, and scissors?: ").lower()
     print(computer)
     print(player)
-    print("")
-
-
-
+  if player==computer:
+    print(computer)
+    print(player)
+    print("TIE!!!!!")
+  elif player=="rock":
+    if computer=="paper":
+      print(computer)
+      print(player)
+      print("You lose!!!!!")  
+    if computer=="scissors":
+      print(computer)
+      print(player)
+      print("You win!!!!!")
+  elif player=="scissors":
+    if computer=="rock":
+      print(computer)
+      print(player)
+      print("You lose!!!!!")  
+    if computer=="paper":
+      print(computer)
+      print(player)
+      print("You win!!!!!")
+  elif player=="paper":
+    if computer=="scissors":
+      print(computer)
+      print(player)
+      print("You lose!!!!!")  
+    if computer=="rock":
+      print(computer)
+      print(player)
+      print("You win!!!!!")
+  play_again=input("Play again? (yes/no): ").lower()
+  if play_again != "yes":
+    break
+print("bye")
 
